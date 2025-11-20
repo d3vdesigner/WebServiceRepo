27d3a0547e0b45cdb839d3a3bbbd4b33c35d885d
Richard Evans Vilanculo <evansvillanculo@gmail.com>
2025-11-20 22:27:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'WebService: Online!'
+  
+@app.route('/status')
+def status_check():
+    return jsonify({
+        "status": "OK",
+        "service": "WebServiceRepo",
+        "version": "1.0"
+    })
+  
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
